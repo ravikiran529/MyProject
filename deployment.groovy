a92149c2cfed06fb88f091a2ab6439a10d8ca790
Ravikiran Rapelli <53438035+ravikiran529@users.noreply.github.com>
2020-07-13 16:42:02 +0530

    Update deployment.groovy

--- deployment.groovy
+++ deployment.groovy
@@ -1,4 +1,4 @@
-def call(qa){
+def qa(){
     
         env.BUILD_ENV = "qa"
         env.APPCENTER_APP = "mssapps-5bfk/CNNgo-Android-TV-QA"
@@ -6,7 +6,7 @@ def call(qa){
         env.PLATFORM = "android"
         env.PATH = "${PATH}:${HOME}/sonar-scanner/bin"
 }
-def call(prd){
+def prd(){
         env.BUILD_ENV = "prd"
         env.APPCENTER_APP = "mssapps-5bfk/CNNgo-Android-TV-QA"
         env.APPCENTER_GROUP = "All-users-of-CNNgo-Android-TV-QA"
